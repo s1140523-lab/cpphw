4e2fd483ff8a84707d0b4a0e334b7d7bf5c84314
s1140523-lab <s1140523@dmhs.kh.edu.tw>
2026-04-22 15:16:04 +0800

    Create  escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    cout << "1快樂星球" << end1; 
+    cout << "2快樂 星球" << end1; 
+    cout << "3快樂\t星球" << end1; 
+    cout << "4快樂\n星球" << end1; 
+    cout << "\\5快樂星球\\" << end1; 
+    cout << "\"6快樂星球\"" << end1; 
+    return 0;
+}
