dbb6ee3fb8185ef098506ded2f6332e84daa91a9
s1140523-lab <s1140523@dmhs.kh.edu.tw>
2026-05-25 09:52:04 +0800

    Create 99table.cpp

---
+++ 99table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; 1  <= 9; i++) {
+    for (int j = 1; j <= 9; j++) {
+   cout << " i " << " * " << j << " = " << 1 * j  << "\t";    
+   }
+   cout << end1;
+    }
+    return 0;
+}
